c0031f4219df82cdbeea5ec0889f7b8ddde9afcf
Taishi Furugo <furu5.tai4.meiwa@gmail.com>
2024-06-22 12:52:39 +0000

    "fix test"

--- kadai1.py
+++ kadai1.py
@@ -1,55 +1,64 @@
 import numpy as np
 
-A = np.array([[1,4],[2,3]])
+A = np.array([[1, 4], [2, 3]])
 
 lamda1 = -1
 lamda2 = 5
 
-v1 = np.array([[-2],[1]])
-v2 = np.array([[1],[1]])
-def Av1(A,v1):
+v1 = np.array([[-2], [1]])
+v2 = np.array([[1], [1]])
+
+
+def Av1(A, v1):
     print("Av1 = ")
-    print(A@v1)
+    print(A @ v1)
     print()
-    return A@v1
+    return A @ v1
 
-def lamda1v1(lamda1,v1):
+
+def lamda1v1(lamda1, v1):
     print("λ1v1 = ")
-    print(lamda1*v1)
+    print(lamda1 * v1)
     print()
-    return lamda1*v1
+    return lamda1 * v1
 
-def Av2(A,v2):
+
+def Av2(A, v2):
     print("Av2 = ")
-    print(A@v2)
+    print(A @ v2)
     print()
     return Av2
 
-def lamda2v2(lamda1,v2):
-    print("λ2v2 = ")
-    print(lamda2*v2)
-    print()
-    return lamda2*v2
 
-def eigenSum(lamda1,lamda2):
-    print("固有値の和:",end="")
-    print(lamda1+lamda2)
+def lamda2v2(lamda1, v2):
+    print("λ2v2 = ")
+    print(lamda2 * v2)
     print()
-    return lamda1+lamda2
+    return lamda2 * v2
+
+
+def eigenSum(lamda1, lamda2):
+    print("固有値の和:", end="")
+    print(lamda1 + lamda2)
+    print()
+    return lamda1 + lamda2
+
 
 def trace(A):
-    print("Aのトレース:",end="")
+    print("Aのトレース:", end="")
     print(np.trace(A))
     print()
     return np.trace(A)
 
-def eigenMult(lamda1,lamda2):
-    print("固有値の積:",end="")
-    print(lamda1*lamda2)
+
+def eigenMult(lamda1, lamda2):
+    print("固有値の積:", end="")
+    print(lamda1 * lamda2)
     print()
-    return lamda1*lamda2
+    return lamda1 * lamda2
+
 
 def det(A):
-    print("Aの行列式:",end="")
+    print("Aの行列式:", end="")
     print(np.linalg.det(A))
     return np.linalg.det(A)
--- test.py
+++ test.py
@@ -1,36 +1,37 @@
 import unittest
-import pytest
+# import pytest
 import numpy as np
 from kadai1 import Av1, Av2, lamda1v1, lamda2v2, eigenSum, eigenMult, trace, det
 A = np.array([[1, 4], [2, 3]])
 
-lamda1 = -1
-lamda2 = 5
+Lamda1 = -1
+Lamda2 = 5
 
-v1 = np.array([[-2],[1]])
-v2 = np.array([[1],[1]])
+v1 = np.array([[-2], [1]])
+v2 = np.array([[1], [1]])
 
 
 class TestMyTask(unittest.TestCase):
-    def Av1_lamda1v1(self):
+    def Av1_Lamda1v1(self):
         val1 = Av1(A, v1)
-        val2 = lamda1v1(lamda1, v1)
+        val2 = lamda1v1(Lamda1, v1)
         self.assertEqual(val1, val2)
 
-    def Av2_lamda2v2(self):
+    def Av2_Lamda2v2(self):
         val1 = Av2(A, v2)
-        val2 = lamda2v2(lamda2, v2)
+        val2 = lamda2v2(Lamda2, v2)
         self.assertEqual(val1, val2)
 
     def TestSumTrace(self):
-        val1 = eigenSum(lamda1, lamda2)
+        val1 = eigenSum(Lamda1, Lamda2)
         val2 = trace(A)
         self.assertEqual(val1, val2)
 
     def TestMultDet(self):
-        val1 = eigenMult(lamda1, lamda2)
+        val1 = eigenMult(Lamda1, Lamda2)
         val2 = det(A)
         self.assertEqual(val1, val2)
+
 
 if __name__ == "__main__":
     unittest.main()
