c3fda9bf070b707207fa0e3c1805e465874b1f63
Taishi Furugo <furu5.tai4.meiwa@gmail.com>
2024-06-22 12:44:11 +0000

    "fix test"

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import pytest
 import unittest
+import pytest
 import numpy as np
-from kadai1 import Av1, Av2,lamda1v1,lamda2v2,eigenSum,eigenMult,trace,det
-A = np.array([[1,4],[2,3]])
+from kadai1 import Av1, Av2, lamda1v1, lamda2v2, eigenSum, eigenMult, trace, det
+A = np.array([[1, 4], [2, 3]])
 
 lamda1 = -1
 lamda2 = 5
@@ -10,21 +10,27 @@ lamda2 = 5
 v1 = np.array([[-2],[1]])
 v2 = np.array([[1],[1]])
 
+
 class TestMyTask(unittest.TestCase):
     def Av1_lamda1v1(self):
-        val1 = Av1(A,v1)
-        val2 = lamda1v1(lamda1,v1)
-        self.assertEqual(val1,val2)
+        val1 = Av1(A, v1)
+        val2 = lamda1v1(lamda1, v1)
+        self.assertEqual(val1, val2)
+
+    def Av2_lamda2v2(self):
+        val1 = Av2(A, v2)
+        val2 = lamda2v2(lamda2, v2)
+        self.assertEqual(val1, val2)
 
     def TestSumTrace(self):
-        val1 = eigenSum(lamda1,lamda2)
+        val1 = eigenSum(lamda1, lamda2)
         val2 = trace(A)
-        self.assertEqual(val1,val2)
+        self.assertEqual(val1, val2)
 
     def TestMultDet(self):
-        val1 = eigenMult(lamda1,lamda2)
+        val1 = eigenMult(lamda1, lamda2)
         val2 = det(A)
-        self.assertEqual(val1,val2)
+        self.assertEqual(val1, val2)
 
 if __name__ == "__main__":
     unittest.main()
